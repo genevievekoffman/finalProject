446f7c0ff89876d3215c3b90d2ca4ba8e39fcdb3
Genevieve Koffman <gkoffma1@ugrad8.cs.jhu.edu>
2021-11-23 22:42:35 -0500

    client and server

---
+++ client.c
@@ -0,0 +1,257 @@
+//client program
+
+#include "sp.h"
+#include "structs.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_USERNAME 20
+
+static void print_emails();
+static void print_menu();
+static void User_command();
+static char Spread_name[80];
+static int To_exit = 0;
+static mailbox Mbox;
+static char Private_group[MAX_GROUP_NAME];
+static char curr_server[10]; //servers private name we are connected to atm
+static char curr_client[MAX_USERNAME]; //username of the current client logged in
+static void Bye();
+static void Read_message();
+
+static int logged_in = 0; //1 means there is a use logged in (I didnt know how to check if currclient was null bc it doesnt work ...
+static email *new_mail;
+
+//internal DS: array holding 20 cells (pointer to cells)
+static cell *client_window[20];
+
+int main( int argc, char *argv[] ) 
+{
+    sp_time test_timeout;
+    test_timeout.sec = 5;
+    test_timeout.usec = 0;
+
+    
+    new_mail = malloc(sizeof(email));
+
+    sprintf( Spread_name, "10050"); //always connects to my port
+    
+    int ret;
+    /* connect to SPREAD */
+    ret = SP_connect_timeout( Spread_name, "client", 0, 1, &Mbox, Private_group, test_timeout ); 
+    if( ret != ACCEPT_SESSION ) {
+        SP_error( ret );
+        Bye();
+    }
+    printf("Client: connected to %s with private group %s\n", Spread_name, Private_group );
+
+    E_init();
+    E_attach_fd( 0, READ_FD, User_command, 0, NULL, HIGH_PRIORITY); //users control has highest priority
+    E_attach_fd( Mbox, READ_FD, Read_message, 0, NULL, LOW_PRIORITY );
+
+    //need to connect to a server 
+    print_menu();
+    for(;;)
+        User_command();
+}
+
+static void User_command()
+{
+    char command[130];
+
+    for(int i = 0; i < sizeof(command); i++) command[i] = 0;
+    fgets(command,130,stdin);
+
+    switch (command[0])
+    {
+        case 'u': ;
+            //logging in
+            char username[MAX_USERNAME];
+            int ret = sscanf( &command[2], "%s", username );
+            if ( ret < 1 ) {
+                printf("\ninvalid username\n");
+                break;
+            }
+
+            //TODO: add further check on if user is already logged in
+            sprintf(curr_client, username);
+
+            //creates its own group username
+            ret = SP_join(Mbox, username);
+            if ( ret < 0 ) SP_error( ret );
+            logged_in = 1;
+            //TODO: must request the data from server to fill data structure
+
+            break;
+
+        case 'c': ;
+            //connecting to a mail server
+            if ( logged_in == 0) {
+                printf("\n...no user logged in ... \n");
+                break;
+            }
+            char server[MAX_USERNAME];
+        
+            ret = sscanf( &command[2], "%s", server );
+            //make sure it's just one int & 1-5
+            int i = (int)(server[0] - '0');
+            if ( ret != 1 || i > 5 || i < 1 ) { 
+                printf("\ninvalid server number\n");
+                break;
+            }
+
+            //joins the server_client group
+            char c = '_';
+            strncat(server, &c, 1);
+            printf("\nserver = %s", server);
+            strncat( server, curr_client, sizeof(curr_client) );
+            printf("\nafter strncat; server_client = %s\n", server);
+            
+            ret = SP_join(Mbox, server);
+            if ( ret < 0 ) SP_error( ret );
+            
+            //requests to connect with that server
+            ret = SP_join(Mbox, &server[0]); //server[0] is the servers index 
+            if ( ret < 0 ) SP_error( ret );
+            
+            //TODO: waits until it gets a membership notification that says the server joined the group
+                //then we can update the curr_server (connection etablished)
+            //sprintf(curr_server, input[0]); 
+             
+            break;
+
+        case 'm': ;
+            //sending an email
+            if ( logged_in == 0) {
+                printf("\n...no user logged in ... \n");
+                break;
+            }
+            
+            //get the recipient/to, subject, & msg
+            sprintf(new_mail->sender, curr_client);
+            printf("\n\tto: ");
+            fgets(new_mail->to,MAX_USERNAME,stdin);
+            printf("\n\tsubject: ");
+            fgets(new_mail->subject,MAX_CHAR,stdin);
+            printf("\n\tmessage: ");
+            fgets(new_mail->message,250,stdin);
+                
+            /*
+            printf("\nnew email:");
+            printf("\n\tto= %s", new_mail->to);
+            printf("\n\tsubject = %s", new_mail->subject);
+            printf("\n\tmsg = %s", new_mail->message);
+            */
+            
+            //TODO: send this email to the servers group
+            //server will create a new update and put the email in it 
+            ret = SP_multicast(Mbox, AGREED_MESS, curr_server, 1, sizeof(email), (char*)new_mail); //tag 1 = new email from client
+
+            //just a test to see if print works
+            /*
+            cell test;
+            test.sn = 1;
+            test.status = 'u';
+            test.contents = new_mail;
+            client_window[0] = &test;
+            */
+            
+            break;
+
+        case 'd': ;
+            if (logged_in == 0) {
+                printf("\n...no user logged in ... \n");
+                break;
+            }
+
+            char mailsn[5];
+        
+            ret = sscanf( &command[2], "%s", mailsn );
+            if (ret < 1 || ret > 2) {
+                printf("\ninvalid serial number\n");
+                break;
+            }
+            //can be an int 1-20
+            
+            //grab the emails id with sn: mailsn
+            //sn is just i-1
+            int sn = atoi(mailsn);
+            printf("sn converted is %d\n", sn);
+            if ( client_window[sn-1] != NULL ) {
+                //set its status to delete
+                //send an update to server
+
+            }
+                
+            break;
+        case 'r':
+
+        case 'l': ;
+            //must be logged in
+            if ( logged_in == 0) {
+                printf("\n...no user logged in ... \n");
+                break;
+            }
+            ret = SP_multicast( Mbox, AGREED_MESS, curr_server, 4, sizeof(curr_client), curr_client); //tag 4 = requesting list of emails to refill our client_window
+            if ( ret < 0 ) {
+                SP_error( ret );
+                Bye();
+            }
+            
+            //testing
+            print_emails();
+            break;
+
+        default:
+            printf("\nUnknown command\n");
+            break;
+    }
+}
+
+static void Read_message()
+{
+
+}
+
+/* prints out contents of client_window */
+static void print_emails()
+{
+    printf("\nuser:%s \nmail server:%s\n", curr_client, curr_server);
+    for( int i = 0; i < 20; i++ ) {
+        cell *email = client_window[i];
+        if(email == NULL) {
+            if (i == 0) printf("\tno mail");
+            return;
+        }
+        printf("\tsn=%d:", email->sn);
+        printf(" sender=%s", email->contents->sender);
+        printf(", subject=%s", email->contents->subject);
+    }
+
+}
+
+static void print_menu()
+{
+    printf("\n");
+    printf("User Menu:\n");
+    printf("----------\n");
+    printf("\n\tu <usernmae> -- login with username\n");
+    printf("\tc <server> -- connect with mail server\n");
+    printf("\tl -- list received mail\n");
+    printf("\tm -- write an email\n");
+    printf("\td <email sn> -- delete an email\n");
+    printf("\tr <email sn> -- read an email\n");
+    printf("\tv -- prints memberships in current mail server\n");
+    fflush(stdout);
+}
+
+static void Bye()
+{
+    To_exit = 1;
+    free(new_mail);
+    printf("\nBye.\n");
+    SP_disconnect( Mbox );
+    exit(0);
+}
---
+++ server.c
@@ -0,0 +1,244 @@
+//server program
+
+#include "structs.h"
+#include "sp.h"
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_SERVERS 5
+#define MAX_VSSETS 10 //?what is this for
+#define MAX_MESSLEN     5000
+
+static char all_servers[] = "all_servers";
+static char server_index; //unique server index
+static char Private_group[MAX_GROUP_NAME];
+static char Server[80];
+static mailbox Mbox;
+static char Spread_name[80];
+static int To_exit = 0;
+static char *buf;
+static int msg_size = sizeof(update);
+
+char sender[MAX_GROUP_NAME]; //either a server or user
+
+static void Read_message();
+static void Bye();
+static void request_mailbox(char *client);
+
+static update *new_update;
+static int updates_sent; //sequence num
+FILE *fw;
+
+int main(int argc, char **argv)
+{
+    sp_time test_timeout;
+    test_timeout.sec = 5;
+    test_timeout.usec = 0;
+
+    updates_sent = 0; //initialize to 0
+    new_update = malloc(sizeof(update));
+    
+    //handle arguments
+    if ( argc != 2 ) {
+        printf("Usage: server <1-5>\n");
+        exit(0);
+    }
+
+    char *str = argv[1];
+    //server_index = atoi(str);
+    server_index = *str; 
+    printf("\nserver_index = %d", server_index);
+    fflush(0);
+    
+    sprintf( Server, "server" );
+    sprintf( Spread_name, "10050"); //always connects to my port
+    strncat(Server, argv[1], 1);
+    printf("\nafter strncat Server = %s\n", Server);
+    
+    int ret;
+    /* connect to SPREAD */ 
+    ret = SP_connect_timeout( Spread_name, Server, 0, 1, &Mbox, Private_group, test_timeout ); //Spread_name is the daemon, Server is what they will be known as,
+                                    //private_group is returned group name(used for unicast msgs only)
+    if( ret != ACCEPT_SESSION ) {
+        SP_error( ret );
+        Bye();
+    }
+    printf("\n\t>Server: connected to %s with private group %s\n", Spread_name, Private_group );
+    //private_group is like #S1#ugrad8
+
+    //joins its own public group ex: S1, S2 ... (clients will request connection here)
+    //ret = SP_join( Mbox, Server );
+    //if ( ret < 0 ) SP_error( ret );
+
+    printf("\nmailbox = %d\n", Mbox);
+    //joins the network group with all servers in it
+    ret = SP_join( Mbox, all_servers);
+    if ( ret < 0 ) SP_error( ret );
+    
+    for(;;)
+        Read_message();
+}
+
+static void Read_message()
+{
+    static  char    mess[MAX_MESSLEN];
+    char            target_groups[MAX_SERVERS][MAX_GROUP_NAME];
+    membership_info memb_info;
+    int             service_type;
+    int16           mess_type;
+    int             endian_mismatch;
+    int             num_groups;
+    vs_set_info     vssets[MAX_VSSETS];
+    int             num_vs_sets;
+    unsigned int    my_vsset_index;
+    char            members[MAX_SERVERS][MAX_GROUP_NAME];
+    int             ret;
+    service_type = 0;
+
+    //can receive from all_servers or any client connected to it
+    ret = SP_receive( Mbox, &service_type, sender, 10, &num_groups, target_groups, &mess_type, &endian_mismatch, msg_size, buf);
+        //target groups should tell us who it was sent to 
+    if ( ret < 0 ) {
+        if ( ! To_exit ) 
+        {
+            SP_error( ret );
+            printf("\nBye.\n");
+        }
+        exit(0);
+    }
+
+    if ( Is_regular_mess( service_type ) )
+    {
+        mess[ret] = 0;
+        switch ( mess_type )
+        {
+            case 0: ;
+                printf("\n0");
+                break;
+            case 1: ;
+                //1 = new email 
+                //is it from a client or another server?
+                //check the sender -> 
+                
+                //if it's from a client: (not server 1 through 5)
+                    //create a new update/fill its data
+                    new_update->type = 1; //new email
+                    new_update->server = server_index;
+                    updates_sent++;
+                    new_update->sequence_num = updates_sent;
+                    email *new_email = (email*)buf;
+
+                    new_update->email_ = *new_email;
+                
+                    char filename[] = "/tmp/ts_";
+                    strcat(filename, &server_index);
+                    strcat(filename, new_email->to);
+
+                    //write the new email to our log/file for that users email file
+                    if ( (fw = fopen( (strcat(filename,"emails.txt") ) , "w") ) == NULL ) {
+                        perror("fopen");
+                        exit(0);
+                    }
+                    //write to the top of the file
+                    fprintf(fw,"%d %d", new_update->server, new_update->sequence_num); //server, sequence_num : to, subject, msg, sender
+                //if it's from another server:
+                printf("\n1");
+                break;
+        }
+        printf("reg msg");
+    } else if (Is_membership_mess( service_type ) )
+    {
+        ret = SP_get_memb_info( buf, service_type, &memb_info );
+        if (ret < 0)
+        {
+            printf("BUG: membership message does not have valid body\n");
+            SP_error( ret );
+            exit(1);
+        }
+        if ( Is_reg_memb_mess( service_type ) )
+        {
+            printf("Received REGULAR membership for group %s with %d members, where I am member %d:\n",
+                sender, num_groups, mess_type );
+            for( int i=0; i < num_groups; i++ )
+                printf("\t%s\n", &target_groups[i][0] );
+            printf("grp id is %d %d %d\n",memb_info.gid.id[0], memb_info.gid.id[1], memb_info.gid.id[2] ); 
+
+            if ( Is_caused_join_mess( service_type ) ){
+                printf("Due to the JOIN of %s\n", memb_info.changed_member );
+            } else if( Is_caused_leave_mess( service_type ) ){
+                printf("Due to the LEAVE of %s\n", memb_info.changed_member );
+            } else if( Is_caused_disconnect_mess( service_type ) ){
+                printf("Due to the DISCONNECT of %s\n", memb_info.changed_member );
+            } else if( Is_caused_network_mess( service_type ) )
+            {
+                printf("Due to NETWORK change with %u VS sets\n", memb_info.num_vs_sets);
+                num_vs_sets = SP_get_vs_sets_info( buf, &vssets[0], MAX_VSSETS, &my_vsset_index );
+                if (num_vs_sets < 0)
+                {
+                    printf("BUG: membership message has more then %d vs sets. Recompile with larger MAX_VSSETS\n", MAX_VSSETS);
+                    SP_error( num_vs_sets );
+                    exit( 1 );
+                }
+                for (int i = 0; i < num_vs_sets; i++ )
+                {
+                    printf("%s VS set %d has %u members:\n",
+                        (i  == my_vsset_index) ?
+                        ("LOCAL") : ("OTHER"), i, vssets[i].num_members );
+                    ret = SP_get_vs_set_members(buf, &vssets[i], members, MAX_SERVERS);
+                    if (ret < 0) {
+                        printf("VS Set has more then %d members. Recompile with larger MAX_MEMBERS\n", MAX_SERVERS);
+                        SP_error( ret );
+                        exit( 1 );
+                    }
+                    for ( int j = 0; j < vssets[i].num_members; j++ )
+                        printf("\t%s\n", members[j] );
+                }
+            }
+        
+        }else if (Is_transition_mess(   service_type ) ) {
+            printf("received TRANSITIONAL membership for group %s\n", sender );
+        }else if( Is_caused_leave_mess( service_type ) ){
+            printf("received membership message that left group %s\n", sender );
+        }else printf("received incorrecty membership message of type 0x%x\n", service_type );
+    
+    } else printf("received message of unknown message type 0x%x with ret %d\n", service_type, ret);
+
+}
+
+/*when a client requests to see their mail, client is the name of the user logged in*/
+static void request_mailbox(char *client)
+{
+    cell new_window[20];
+    int sn = 1; //goes up to 20
+    FILE *fr; //pointer to file for reading 
+    char filename[] = "/tmp/ts_";
+    strcat(filename, &server_index);
+    strcat(filename, client);
+    
+    while ( sn <= 20 ) { 
+        //open their emails file 
+        if ( (fr = fopen( (strcat(filename,"emails.txt") ) , "r") ) == NULL ) {
+            //nothing to read
+            //new_window[0] = NULL;
+            break;
+        }
+        //go to eof
+        //open serverindex_client_emails file
+        //unique id will be first
+    }
+
+    //send the new_window to the client
+//    int ret = SP_multicast(Mbox, AGREED_MESS,
+}
+
+static void Bye()
+{
+    To_exit = 1;
+    free(new_update);
+    printf("\nBye.\n");
+    SP_disconnect( Mbox );
+    exit(0);
+}
